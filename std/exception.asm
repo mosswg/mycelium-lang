301a29e68edd3d57f40772b629b52757fe67cb6d
Moss Gallagher <moss@mossx.net>
2021-10-15 00:31:17 -0400

    std/exception: Added exceptions for arr and type

--- std/exception.asm
+++ std/exception.asm
@@ -14,7 +14,12 @@
     exception~runtime~bad_arg:                      equ 1
     exception~runtime~bad_arg_str:                  db "Invalid Argument Index", 0xA
     exception~runtime~bad_arg_len:                  equ $-exception~runtime~bad_arg_str
-
+    exception~runtime~bad_index:                    equ 2
+    exception~runtime~bad_index_str:                db "Invalid Array Index", 0xA
+    exception~runtime~bad_index_len:                equ $-exception~runtime~bad_arg_str
+    exception~runtime~bad_type:                     equ 3
+    exception~runtime~bad_type_str:                 db "Unknown Type", 0xA
+    exception~runtime~bad_type_len:                 equ $-exception~runtime~bad_type_str
 
 ; Args
 ;   rax: the exception number
@@ -24,20 +29,30 @@ exception~runtime~throw:
     push    rax
 
     cmp     rax, exception~runtime~bad_arg
-    je      .case_0
-
+    je      .case_bad_arg
+    cmp     rax, exception~runtime~bad_index
+    je      .case_bad_index
+    cmp     rax, exception~runtime~bad_type
+    je      .case_bad_type
     jmp     .default
 
-    .case_0:
+    .case_bad_arg:
         mov     rax, exception~runtime~bad_arg_str
         mov     rbx, exception~runtime~bad_arg_len
-        call    out~puts
+        jmp     .switch_end
+    .case_bad_index:
+        mov     rax, exception~runtime~bad_index_str
+        mov     rbx, exception~runtime~bad_index_len
+        jmp     .switch_end
+    .case_bad_type:
+        mov     rax, exception~runtime~bad_type_str
+        mov     rbx, exception~runtime~bad_type_len
         jmp     .switch_end
     .default:
         mov     rax, exception~runtime~bad_exception_number_str
         mov     rbx, exception~runtime~bad_exception_number_len
-        call    out~puts
     .switch_end:
+    call    out~puts
     pop     rax
     call sys~exit
     ret
