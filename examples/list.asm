9c7308ac278187ead7c770c25ebbb501b0d39f28
Moss Gallagher <moss@mossx.net>
2021-10-17 23:30:46 -0400

    Examples: Renamed arr to list
    Renamed and we're using new names.

--- examples/list.asm
+++ examples/list.asm
@@ -4,7 +4,7 @@
 %include "std/sys.asm"
 %include "std/arg.asm"
 %include "std/str.asm"
-%include "std/arr.asm"
+%include "std/list.asm"
 %include "std/int.asm"
 %include "std/type.asm"
 
@@ -12,13 +12,16 @@
 global _start
 
 section .data
-  err:              db "Usage: ./arg <size>", 0
-  create_success:   db "Created array with size ", 0
-  write_success:    db "Wrote Values to array: ", 0
-  resize_success:   db "Resized array to size ", 0
-  success_post:     db " with an address of ", 0
-  first_size:       equ 8
-  new_size:         equ 16
+  usage:              db "Usage: ./arg <size>", 0
+  create_success:     db "Created list with size ", 0
+  write_success:      db "Wrote Values to list: ", 0
+  push_success_pre:   db "Push value ", 0
+  push_success_post:  db " to array: ", 0
+  resize_success:     db "Resized list to size ", 0
+  success_post:       db " with an address of ", 0
+  first_size:         equ 8
+  new_size:           equ 16
+  push_value:         equ 10
 
 section .text
 
@@ -29,17 +32,17 @@ _start:
   call  sys~exit                ; call exit
 
 main:
-  mov   rax, arr#type#basic
+  mov   rax, list#type#basic
   mov   rbx, first_size
 
-  call  arr#new
+  call  list#new
   mov   r9, rsi
 
   mov   rax, create_success
   call  str~print
 
   mov   rax, r9
-  call  arr~len
+  call  list~len
 
   mov   rax, rsi
   call  int~print
@@ -51,7 +54,7 @@ main:
   call  int~println
 
   mov   rax, r9
-  call  arr~println
+  call  list~println
 
   mov   rax, 0xA
   call  out~putc
@@ -62,36 +65,54 @@ main:
       mov   rax, r9
       mov   rcx, rbx
       mov   rdx, type#int
-      call  arr~set
+      call  list~set
       add   rbx, 1
   .loop_check:
       cmp   rbx, first_size
       jl    .loop
 
-  mov   rax, r9
-  mov   rbx, 10
-  call  arr~push                ; rax.push(10)
-  mov   r9, rax
 
   mov   rax, write_success
   call  str~print
 
   mov   rax, r9
-  call  arr~println
+  call  list~println
+
+  mov   rax, 0xA
+  call  out~putc
+
+
+  mov   rax, r9
+  mov   rbx, push_value
+  call  list~push
+  mov   r9, rax
+
+  mov   rax, push_success_pre
+  call  str~print
+
+  mov   rax, push_value
+  call  int~print
+
+  mov   rax, push_success_post
+  call  str~print
+
+  mov   rax, r9
+  call  list~println
+
 
   mov   rax, 0xA
   call  out~putc
 
   mov   rax, r9
   mov   rbx, new_size
-  call  arr~resize
+  call  list~resize
   mov   r9, rax
 
   mov   rax, resize_success
   call  str~print
 
   mov   rax, r9
-  call  arr~len
+  call  list~len
 
   mov   rax, rsi
   call  int~print
@@ -103,10 +124,10 @@ main:
   call  int~println
 
   mov   rax, r9
-  call  arr~println
+  call  list~println
 
   mov   rax, r9
-  call  arr~del
+  call  list~del
 
   mov   rsi, 0
   ret
