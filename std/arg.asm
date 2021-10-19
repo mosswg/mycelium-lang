daadfe66a8572060ef674c8baa5a8d5a6c6c1013
Moss Gallagher <moss@mossx.net>
2021-10-18 20:43:54 -0400

    std/arg: Added require_args Function
    This should later be replaced with a more suitable function where
    arguments are registered then store in a hashmap or similar type.
    Currently the types that would be needed to impletent this do not yet
    exist.
    Also changed arg~argc to arg~number_args

--- std/arg.asm
+++ std/arg.asm
@@ -5,6 +5,11 @@
 %define _Mycelium_std_arg_
 
 %include "std/exception.asm"
+%include "std/str.asm"
+
+
+    usage:              db "Usage: ", 0
+
 
 ; Args
 ;   rax: the index of the args
@@ -27,10 +32,57 @@ arg~get:
     ret
 
 ; Args
+;   rax:    the amount of required args
+;   stack:  names of args
+; Returns
+;   void
+arg~require_args:
+    cmp     rax, [r15]
+    jl     .valid_args
+
+    mov     r9, rax             ; We don't need to push because if we're here we're going to exit the program and therefore we don't care.
+
+    mov     rax, usage
+    call    str~print
+
+    mov     rax, [r15+8]
+    call    str~print
+
+    mov     rbx, 0
+    mov     rcx, 8
+
+    .arg_loop:
+        mov     rax, ' '
+        call    out~putc
+
+        mov     rax, rbx
+        mul     rcx
+        mov     r10, rsp
+        add     r10, 8
+        add     r10, rax
+
+        mov     rax, [r10]
+        call    str~print
+
+        add     rbx, 1
+    .arg_loop_check:
+        cmp     rbx, r9
+        jl      .arg_loop
+
+
+    mov     rax, 0xA
+    call    out~putc
+
+    mov     rax, 1
+    call    sys~exit
+    .valid_args:
+    ret
+
+; Args
 ;   void
 ; Returns
 ;   rsi: Number of args
-arg~argc:
+arg~number_args:
     mov     rsi, [r15]
     ret
 
