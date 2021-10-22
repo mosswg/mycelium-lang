7c7da3afc85bfc00d2040fdb894a7abd96aa9ad8
Moss Gallagher <moss@mossx.net>
2021-10-21 21:55:07 -0400

    std/exception: Added bad_token

--- std/exception.asm
+++ std/exception.asm
@@ -6,25 +6,28 @@
 
 %include "std/sys.asm"
 %include "std/out.asm"
-
+%include "std/str.asm"
 
 
     ;;  TODO: Change this to an indexable array or list.
     exception~runtime~bad_exception_number:         equ 0
-    exception~runtime~bad_exception_number_str:     db "Invalid Exception Number", 0xA, 0
+    exception~runtime~bad_exception_number_str:     db "Invalid Exception Number", 0xa
     exception~runtime~bad_exception_number_len:     equ $-exception~runtime~bad_exception_number_str
     exception~runtime~bad_arg:                      equ 1
-    exception~runtime~bad_arg_str:                  db "Invalid Argument Index", 0xA
+    exception~runtime~bad_arg_str:                  db "Invalid Argument Index", 0xa
     exception~runtime~bad_arg_len:                  equ $-exception~runtime~bad_arg_str
     exception~runtime~bad_index:                    equ 2
-    exception~runtime~bad_index_str:                db "Invalid Index", 0xA
+    exception~runtime~bad_index_str:                db "Invalid Index", 0xa
     exception~runtime~bad_index_len:                equ $-exception~runtime~bad_index_str
     exception~runtime~bad_type:                     equ 3
-    exception~runtime~bad_type_str:                 db "Unknown Type", 0xA
+    exception~runtime~bad_type_str:                 db "Unknown Type", 0xa
     exception~runtime~bad_type_len:                 equ $-exception~runtime~bad_type_str
     exception~runtime~bad_allocation:               equ 4
-    exception~runtime~bad_allocation_str:           db "Could Not Allocate Memory"
+    exception~runtime~bad_allocation_str:           db "Could Not Allocate Memory", 0xa
     exception~runtime~bad_allocation_len:           equ $-exception~runtime~bad_allocation_str
+    exception~runtime~bad_token:                    equ 5
+    exception~runtime~bad_token_str:                db "Unknown Token: "
+    exception~runtime~bad_token_len:                equ $-exception~runtime~bad_token_str
 
 
 ; Args
@@ -42,31 +45,45 @@ exception~runtime~throw:
     je      .case_bad_type
     cmp     rax, exception~runtime~bad_allocation
     je      .case_bad_alloc
+    cmp     rax, exception~runtime~bad_token
+    je      .case_bad_token
     jmp     .default
 
     .case_bad_arg:
         mov     rax, exception~runtime~bad_arg_str
         mov     rbx, exception~runtime~bad_arg_len
+        call    out~put_err
         jmp     .switch_end
     .case_bad_index:
         mov     rax, exception~runtime~bad_index_str
         mov     rbx, exception~runtime~bad_index_len
+        call    out~put_err
         jmp     .switch_end
     .case_bad_type:
         mov     rax, exception~runtime~bad_type_str
         mov     rbx, exception~runtime~bad_type_len
+        call    out~put_err
         jmp     .switch_end
     .case_bad_alloc:
         mov     rax, exception~runtime~bad_allocation_str
         mov     rbx, exception~runtime~bad_allocation_len
+        call    out~put_err
+        jmp     .switch_end
+    .case_bad_token:
+        mov     rax, exception~runtime~bad_token_str
+        mov     rbx, exception~runtime~bad_token_len
+        call    out~put_err
+        mov     rax, rbx
+        call    str~print
         jmp     .switch_end
     .default:
         mov     rax, exception~runtime~bad_exception_number_str
         mov     rbx, exception~runtime~bad_exception_number_len
+        call    out~put_err
     .switch_end:
-    call    out~put_err
     pop     rax
-    call sys~exit
+    neg     rax
+    call    sys~exit
     ret
 
 
