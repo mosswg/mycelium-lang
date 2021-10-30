f4179b22cbfcee9339a04dcb268ea8322086bc3d
Moss Gallagher <moss@mossx.net>
2021-10-30 13:02:13 -0400

    std/exception: Added compiletime.bad_op_arg

--- std/exception.asm
+++ std/exception.asm
@@ -13,18 +13,23 @@
     exception~runtime~bad_exception_number:         equ 0
     exception~runtime~bad_exception_number_str:     db "Invalid Exception Number", 0xa
     exception~runtime~bad_exception_number_len:     equ $-exception~runtime~bad_exception_number_str
+
     exception~runtime~bad_arg:                      equ 1
     exception~runtime~bad_arg_str:                  db "Invalid Argument Index", 0xa
     exception~runtime~bad_arg_len:                  equ $-exception~runtime~bad_arg_str
+
     exception~runtime~bad_index:                    equ 2
     exception~runtime~bad_index_str:                db "Invalid Index", 0xa
     exception~runtime~bad_index_len:                equ $-exception~runtime~bad_index_str
+
     exception~runtime~bad_type:                     equ 3
     exception~runtime~bad_type_str:                 db "Unknown Type", 0xa
     exception~runtime~bad_type_len:                 equ $-exception~runtime~bad_type_str
+
     exception~runtime~bad_allocation:               equ 4
     exception~runtime~bad_allocation_str:           db "Could Not Allocate Memory", 0xa
     exception~runtime~bad_allocation_len:           equ $-exception~runtime~bad_allocation_str
+
     exception~runtime~not_implemented:              equ 6
     exception~runtime~not_implemented_str:          db "Not Implemented: ", 0xa
     exception~runtime~not_implemented_len:          equ $-exception~runtime~not_implemented_str
@@ -73,7 +78,7 @@ exception~runtime~throw:
         mov     rbx, exception~runtime~not_implemented_len
         call    out~put_err
         mov     rax, rbx
-        call    str~print
+        call    cstr~print
         jmp     .switch_end
     .default:
         mov     rax, exception~runtime~bad_exception_number_str
@@ -86,12 +91,20 @@ exception~runtime~throw:
     ret
 
 
+    exception~compiletime~pre:                          db "Compiler Error: ", 0
+    exception~compiletime~pre_len:                      equ $-exception~compiletime~pre
+
     exception~compiletime~bad_exception_number:         equ 0
     exception~compiletime~bad_exception_number_str:     db "Invalid Exception Number", 0xa
     exception~compiletime~bad_exception_number_len:     equ $-exception~compiletime~bad_exception_number_str
+
     exception~compiletime~bad_token:                    equ 0x1
-    exception~compiletime~bad_token_str:                db "Unknown Token: ", 0xa
+    exception~compiletime~bad_token_str:                db "Unknown Token ", 0
     exception~compiletime~bad_token_len:                equ $-exception~compiletime~bad_token_str
+
+    exception~compiletime~bad_op_arg:                   equ 0x2
+    exception~compiletime~bad_op_arg_str:               db " Operator Expected Value Argument but Found ", 0
+    exception~compiletime~bad_op_arg_len:               equ $-exception~compiletime~bad_op_arg_str
 
 
 
@@ -102,23 +115,44 @@ exception~runtime~throw:
 ;   void
 exception~compiletime~throw:
     ;; Switch
-    cmp     rax, exception~compiletime~bad_token
+    mov     r9, rax
+    mov     r10, rbx
+    mov     r11, rcx
+
+    mov     rax, exception~compiletime~pre
+    mov     rbx, exception~compiletime~pre_len
+    call    out~put_err
+
+
+    cmp     r9, exception~compiletime~bad_token
     je      .case_bad_token
-
-
+    cmp     r9, exception~compiletime~bad_op_arg
+    je      .case_bad_op_arg
+    jmp     .default
 
     .case_bad_token:
         mov     rax, exception~compiletime~bad_token_str
         mov     rbx, exception~compiletime~bad_token_len
         call    out~put_err
-        mov     rax, rbx
-        call    str~print
+        mov     rax, r10
+        call    cstr~print
+        jmp     .switch_end
+    .case_bad_op_arg:
+        mov     rax, r10
+        call    cstr~print
+        mov     rax, exception~compiletime~bad_op_arg_str
+        mov     rbx, exception~compiletime~bad_op_arg_len
+        call    out~put_err
+        mov     rax, r11
+        call    cstr~print
         jmp     .switch_end
     .default:
         mov     rax, exception~compiletime~bad_exception_number_str
         mov     rbx, exception~compiletime~bad_exception_number_len
         call    out~put_err
     .switch_end:
+    mov     rax, 0xa
+    call    out~putc
     pop     rax
     neg     rax
     call    sys~exit
