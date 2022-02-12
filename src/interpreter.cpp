ea12c7ae9f37e4c4a2e89990ad591fd25e7a4c9a
Moss Gallagher <moss@mossx.net>
2022-02-12 15:48:20 -0500

    Interpreter: Added Basic File Reading

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,7 +1,33 @@
 //
 // Created by moss on 2/11/22.
 //
+#include "base.h"
+#include <fstream>
+#include <vector>
 
 int main(int argc, char** argv) {
 
+	if (argc == 1) {
+		std::cout << "ERROR: " << "No File Given";
+
+		exit(1);
+	}
+
+	std::ifstream in_file(argv[1]);
+
+	std::string line;
+
+	std::vector<std::string> program;
+
+	while (true) {
+		getline(in_file, line);
+		program.emplace_back(line);
+		if (!in_file.good()) {
+			break;
+		}
+	}
+
+
+
+	return 0;
 }
