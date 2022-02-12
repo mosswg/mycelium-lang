ccd50ebcacb720170140ed0a325c4059bdab58c1
Moss Gallagher <moss@mossx.net>
2022-02-12 15:38:43 -0500

    Base: Added Type Class

--- src/base.h
+++ src/base.h
@@ -3,6 +3,33 @@
 //
 #pragma once
 
-class base {
+#include <string>
+#include <iostream>
 
+class type {
+	public:
+		const int pointer = 0;
+		const int integer = 1;
+		const int character = 2;
+		const int string = 3;
+		const int list = 4;
+		const int tuple = 5;
+		const int oper = 6;
+		const int array = 7;
+		const int cstring = 8;
+
+		std::string names[9] = {"pointer", "integer", "char", "string", "list", "tuple", "oper", "array", "cstring"};
+
+		int size[9] = {8, 8, 1, 8, 8, 8, 8, 8, 8};
+};
+
+
+class list {
+	private:
+		void* storage;
+
+	public:
+		list(int length) {
+
+		}
 };
