7417a12755020f607bbe3aff1b54caad6d119971
Moss Gallagher <moss@mossx.net>
2022-03-30 14:44:41 -0400

    Reworked: Tokenizer Token String Lists

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -4,6 +4,15 @@
 
 #include "tokenizer.h"
 
+const std::vector<std::string> mycelium::tokenizer::whitespace_token_strings = {" ", "	"};
+const std::vector<std::string> mycelium::tokenizer::grouping_token_strings = {"(", ")", "{", "}", "[", "]"};
+const std::vector<std::string> mycelium::tokenizer::keyword_token_strings = {"fn", "op", "cn"};
+const std::vector<std::string> mycelium::tokenizer::oper_token_strings = {"+", "-", "*", "/", "%", "=", "==", "!=", "<", "<=", ">", ">=", "&&", "||", "++", "--", "!", "<<", ">>", "&", "|", "~"};
+const std::vector<std::vector<std::string>> mycelium::tokenizer::token_string_lists = {oper_token_strings, whitespace_token_strings, grouping_token_strings, keyword_token_strings};
+
+
+
+
 void mycelium::tokenizer::tokenize() {
 
 	std::vector<std::vector<std::string>> split_lines;
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -15,21 +15,25 @@ namespace mycelium {
 			std::vector<mycelium::token> tokens;
 			std::vector<std::string> lines;
 
-			const std::vector<std::string> math_token_strings = {"+", "-", "*", "/", "%", "="};
-			const std::vector<std::string> logic_token_strings = {"==", "!=", "<", "<=", ">", ">=", "&&", "||"};
-			const std::vector<std::string> unary_token_strings = {"++", "--", "!"};
-			const std::vector<std::string> bitwise_token_strings = {"<<", ">>", "&", "|", "~"};
-			const std::vector<std::string> whitespace_token_strings = {" ", "	"};
-			const std::vector<std::string> grouping_token_strings = {"(", ")", "{", "}", "[", "]"};
-			const std::vector<std::string> keyword_token_strings = {"fn", "op", "cn"};
-			const std::vector<std::vector<std::string>> token_string_lists = {math_token_strings, logic_token_strings, unary_token_strings, bitwise_token_strings, whitespace_token_strings, grouping_token_strings, keyword_token_strings};
-			const std::vector<std::string> token_strings = {"++", "--", "+", "-", "*", "/", "%", "=", "==", "!=", "<<", "<=", "<", ">>", ">=", ">", "&&", "||", "!", "&", "|", "~", " ", "	", "(", ")", "{", "}", "[", "]", "fn", "op", "cn"};
-
-
 		public:
 
+			static const std::vector<std::string> whitespace_token_strings;
+			static const std::vector<std::string> grouping_token_strings;
+			static const std::vector<std::string> keyword_token_strings;
+			static const std::vector<std::string> oper_token_strings;
+			static const std::vector<std::vector<std::string>> token_string_lists;
+			static std::vector<std::string> token_strings;
 
-			explicit tokenizer(std::vector<std::string>  lines) : lines(std::move(lines)) {
+
+
+		explicit tokenizer(std::vector<std::string>  lines) : lines(std::move(lines)) {
+				if (token_strings.empty()) {
+					for (const auto& token_string_list : token_string_lists) {
+						for (const auto& token_string : token_string_list) {
+							token_strings.push_back(token_string);
+						}
+					}
+				}
 				this->tokenize();
 			}
 
