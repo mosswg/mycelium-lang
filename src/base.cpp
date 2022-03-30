2ada6efd6138763821ceeb19d77935431a19e6f1
Moss Gallagher <moss@mossx.net>
2022-03-29 23:25:05 -0400

    Changed: string_split from char to string
    Now string_split uses a vector of strings rather than a vector of
    characters for where to split.

--- src/base.cpp
+++ src/base.cpp
@@ -23,21 +23,41 @@ bool mycelium::vector_contains(const std::vector<T>& vec, T con) {
 	return false;
 }
 
-std::vector<std::string> mycelium::string_split(const std::string& str, const std::vector<char>& spl) {
-	std::vector<std::string> out;
-
-	int start = 0;
-
-	for (int i = 0; i < str.length(); i++) {
-		// std::cout << str[i] << ": " << i << ": " << start << ": " << (vector_contains(spl, str[i])) << "\n";
-		if (vector_contains(spl, str[i])) {
-			out.push_back(str.substr(start, i));
-			start = i+1;
-		}
+std::vector<std::string> mycelium::string_split(const std::string& str, const std::vector<std::string>& spl) {
+	if (str.empty()) {
+		return {};
 	}
 
-	if (vector_contains(spl, (char)1)) {
-		out.push_back(str.substr(start, str.length()));
+	std::vector<std::string> out;
+
+	size_t pos;
+	size_t prev_pos = 0;
+
+	while (prev_pos < str.length()) {
+
+		size_t min_pos = -1;
+		std::string split;
+
+		for (auto &comp: spl) {
+			if ((pos = str.find(comp, prev_pos)) != std::string::npos) {
+				if (pos < min_pos || min_pos == -1) {
+					min_pos = pos;
+					split = comp;
+				}
+			}
+		}
+
+		if (min_pos == -1) {
+			break;
+		}
+
+		if (min_pos != prev_pos) {
+			out.push_back(str.substr(prev_pos, min_pos - prev_pos));
+		}
+
+		out.push_back(split);
+
+		prev_pos = min_pos + split.length();
 	}
 
 	return out;
