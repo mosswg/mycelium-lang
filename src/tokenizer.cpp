91d4696d2e130c7e17f6e78d67db3c75fde807a3
Moss Gallagher <moss@mossx.net>
2022-03-29 23:28:09 -0400

    Added: Start of Tokenize Function
    Currently only splits the lines by the tokens.

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "tokenizer.h"
+
+void mycelium::tokenizer::tokenize() {
+
+	std::vector<std::vector<std::string>> split_lines;
+
+	for (auto& line : lines) {
+		std::cout << "\"" << line << "\"" << "\n\n";
+
+		split_lines.push_back(string_split(line, token_strings));
+	}
+
+	std::cout << split_lines << "\n\n";
+
+}
