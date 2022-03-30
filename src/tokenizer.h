4d31bbfa59b2750c4fa74ff00de849c7af700272
Moss Gallagher <moss@mossx.net>
2022-03-29 23:27:29 -0400

    Added: Token String Vectors
    Also added lines to be a class object of tokenizer.

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -4,21 +4,35 @@
 
 #pragma once
 
+#include <utility>
+
 #include "base.h"
 
 namespace mycelium {
+
 	class tokenizer {
 		private:
 			std::vector<mycelium::token> tokens;
+			std::vector<std::string> lines;
+
+			const std::vector<std::string> math_token_strings = {"+", "-", "*", "/", "%", "="};
+			const std::vector<std::string> logic_token_strings = {"==", "!=", "<", "<=", ">", ">=", "&&", "||"};
+			const std::vector<std::string> unary_token_strings = {"++", "--", "!"};
+			const std::vector<std::string> bitwise_token_strings = {"<<", ">>", "&", "|", "~"};
+			const std::vector<std::string> whitespace_token_strings = {" ", "	"};
+			const std::vector<std::string> grouping_token_strings = {"(", ")", "{", "}", "[", "]"};
+			const std::vector<std::string> keyword_token_strings = {"fn", "op", "cn"};
+			const std::vector<std::vector<std::string>> token_string_lists = {math_token_strings, logic_token_strings, unary_token_strings, bitwise_token_strings, whitespace_token_strings, grouping_token_strings, keyword_token_strings};
+			const std::vector<std::string> token_strings = {"++", "--", "+", "-", "*", "/", "%", "=", "==", "!=", "<<", "<=", "<", ">>", ">=", ">", "&&", "||", "!", "&", "|", "~", " ", "	", "(", ")", "{", "}", "[", "]", "fn", "op", "cn"};
+
 
 		public:
 
-			explicit tokenizer(const std::vector<std::string>& lines) {
-				this->tokenize(lines);
+
+			explicit tokenizer(std::vector<std::string>  lines) : lines(std::move(lines)) {
+				this->tokenize();
 			}
 
-			void tokenize(const std::vector<std::string>& lines) {
-				// TODO: Implement
-			}
+			void tokenize();
 	};
 }
