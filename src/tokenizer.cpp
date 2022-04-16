304f38e556bf25302fa14639d19ee2dc03440fff
Moss Gallagher <moss@mossx.net>
2022-04-15 20:09:27 -0400

    Updated: tokenizer tokenize function

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -6,19 +6,39 @@
 
 void mycelium::tokenizer::tokenize() {
 
+	std::cout << lines << std::endl;
+
 	std::vector<std::vector<std::string>> split_lines;
 
+	std::vector<std::string> split_groups = {};
+
+	split_groups.insert(split_groups.end(), token::whitespace_strings.begin(), token::whitespace_strings.end());
+	split_groups.insert(split_groups.end(), token::grouping_strings.begin(), token::grouping_strings.end());
+
 	for (auto& line : lines) {
-		split_lines.push_back(string_split(line, mycelium::token::strings));
+		split_lines.push_back(string_split(line, split_groups));
 	}
 
 	for (auto& line : split_lines) {
-		for (auto& token_string : line) {
+		for (int i = 0; i < line.size(); i++) {
+			std::string token_string = line[i];
 			token_type type = token::find_type(token_string);
 			if (type != whitespace) {
-				tokens.emplace_back(type, token_string);
+				if (token_string == "<" && line[i + 1] == "<") {
+					tokens.emplace_back(op, "<<");
+					i++;
+				}
+				else if (token_string == ">" && line[i + 1] == ">") {
+					tokens.emplace_back(op, ">>");
+					i++;
+				}
+				else {
+					tokens.emplace_back(type, token_string);
+				}
 			}
 		}
-		tokens.emplace_back(token_type::newline, "");
+		if (tokens.back().type != newline) {
+			tokens.emplace_back(newline, "");
+		}
 	}
 }
